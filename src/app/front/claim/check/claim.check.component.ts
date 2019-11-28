55b79edff8c1da1fef06cc2e4d92615d24e72d20
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-11-28 01:12:09 +0100

    [FRONT] claim check by code

--- src/app/front/claim/check/claim.check.component.ts
+++ src/app/front/claim/check/claim.check.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
-import { ClaimService } from 'src/app/services/managers/claim.service';
+import {ClaimService} from 'src/app/services/managers/claim.service';
 import {Router} from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {StatusModalComponent} from './statusModal/statusModal.component';
 
 @Component({
   selector: 'app-claim-check',
@@ -12,41 +14,40 @@ export class ClaimCheckComponent {
 
   title = 'Consul Claim';
   status;
-  claim;
-  constructor(private claimService: ClaimService, private router: Router) {
+
+  constructor(private claimService: ClaimService, private router: Router, private modalService: NgbModal) {
   }
 
 
   getStatusFrom = new FormGroup({
     claimCode: new FormControl('', [Validators.required]),
   });
+  /*this.claimService.getFaqById(1)
+    .subscribe(
+      response => {
+        console.log('hello faq');
 
-  ngOnInit() {
-    
-  }
-    /*this.claimService.getFaqById(1)
-      .subscribe(
-        response => {
-          console.log('hello faq');
+        console.log(response);
+        } ,
+      error =>{
+        console.log('hello error');
+        console.log(error);
+      }
+  );*/
 
-          console.log(response);
-          } ,
-        error =>{
-          console.log('hello error');
-          console.log(error);
-        } 
-    );*/
-  
 
   getClaimStatus() {
-        this.claimService.getStatudClaimByCode(this.getStatusFrom.value.claimCode)
-      .subscribe( response => {
+    this.claimService.getStatudClaimByCode(this.getStatusFrom.value.claimCode)
+      .subscribe(
+        response => {
           this.status = response['claimStatus'];
-          console.log(this.status.claimStatus)
-        } ,
-        error =>{
-          console.log(error);
-        } 
-    );
+          const modalRef = this.modalService.open(StatusModalComponent);
+          modalRef.componentInstance.status = this.status;
+        },
+        error => {
+          const modalRef = this.modalService.open(StatusModalComponent);
+          modalRef.componentInstance.status = 'Code invalide';
+        }
+      );
   }
 }
