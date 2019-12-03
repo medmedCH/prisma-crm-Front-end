1049dd79a4c4a79ea1cb18c8f4c5238e98041631
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-12-03 15:31:45 +0100

    FAQ

---
+++ src/app/front/FAQ/detail/detail.faq.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {FaqService} from '../../../services/managers/faq.service';
+import { ActivatedRoute } from '@angular/router';
+import {Claim} from '../../../models/Claim';
+import {ClaimService} from '../../../services/managers/claim.service';
+import {NoteClaim} from '../../../models/NoteClaim';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'faq-detail',
+  templateUrl: './detail.faq.component.html',
+  styleUrls: ['./detail.faq.component.css']
+})
+export class DetailFaqComponent {
+  c: {} = {} ;
+
+  notes: NoteClaim[];
+  id = this.route.snapshot.params.id;
+
+  constructor(private claimService: ClaimService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.claimService.getFaqById(this.id)
+      .subscribe(
+        response => {this.c = response;} ,
+        error => {console.log(error);}
+      );
+
+    // @ts-ignore
+    this.claimService.getAllNotesByClaimId<Claim[]>(this.id)
+      .subscribe(
+        response => {this.notes = response;} ,
+        error => {console.log(error);}
+      );
+  }
+}
