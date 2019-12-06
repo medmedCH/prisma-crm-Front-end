3cdff5fc92401fe6f1571c0eef62eb341c19abda
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-12-06 16:55:13 +0100

    add noteClaim Service

---
+++ src/app/services/managers/notesClaim.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {Claim} from '../../models/Claim';
+import {StorageService} from '../security/storage.service';
+import {NoteClaim} from '../../models/NoteClaim';
+
+const httpOptions =  {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT'}),
+  responseType: 'text' as 'text'
+};
+
+@Injectable()
+export class NotesClaimService {
+
+  constructor(private http: HttpClient) {
+  }
+  claimURL = 'http://localhost:9080/prisma-crm-web'
+
+  deleteNoteClaim(n: NoteClaim ) {
+    return this.http.delete(this.claimURL + '/dashboard/reclamation/deleteNote/' + n.id);
+  }
+
+}
