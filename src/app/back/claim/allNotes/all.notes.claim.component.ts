1488407c94a7da99d047542bbad749de945d577a
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-12-06 16:55:38 +0100

    add Delete action for NoteClaim

--- src/app/back/claim/allNotes/all.notes.claim.component.ts
+++ src/app/back/claim/allNotes/all.notes.claim.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {Claim} from '../../../models/Claim';
 import {ClaimService} from '../../../services/managers/claim.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {NoteClaim} from '../../../models/NoteClaim';
+import {StorageService} from '../../../services/security/storage.service';
+import {NotesClaimService} from '../../../services/managers/notesClaim.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -15,8 +17,19 @@ import {NoteClaim} from '../../../models/NoteClaim';
 export class AllNotesClaimComponent implements OnInit {
   @Input()
   notes;
+  @Output()
+  sendDeleteRequest = new EventEmitter();
+  userLogged = StorageService.get('currentUser').userId;
+
+  constructor(private notesClaimService: NotesClaimService) {
+
+  }
+
 
   ngOnInit() {
   }
 
+  deleteNote(n: NoteClaim) {
+    this.sendDeleteRequest.emit(n);
+  }
 }
