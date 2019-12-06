cc0b7769248bc88c6fd84d7b174f9242a81f260a
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-12-06 16:06:05 +0100

    canActivateChild AuthGuard

--- src/app/services/security/auth.guard.ts
+++ src/app/services/security/auth.guard.ts
@@ -17,4 +17,15 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/login']);
     return false;
   }
+
+  canActivateChild() {
+    if (LoginService.isLogged()) {
+      // logged in so return true
+      return true;
+    }
+    // not logged in so redirect to login page
+    this.router.navigate(['/login']);
+    return false;
+  }
+
 }
