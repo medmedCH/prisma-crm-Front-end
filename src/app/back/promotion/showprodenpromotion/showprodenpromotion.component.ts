65ff0f523cf396f3fa2d28b762992df731dc30df
meddd <mohamed.chabbouh@esprit.tn>
2019-12-07 10:25:13 -0800

    mise à jour prmtion

---
+++ src/app/back/promotion/showprodenpromotion/showprodenpromotion.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {PromotionService} from '../../../services/managers/promotion.service';
+import {Product} from '../../../models/Product';
+
+@Component({
+  selector: 'app-showprodenpromotion',
+  templateUrl: './showprodenpromotion.component.html',
+  styleUrls: ['./showprodenpromotion.component.scss']
+})
+export class ShowprodenpromotionComponent implements OnInit {
+  prd: Product[] = [];
+
+  constructor(private promotionservice: PromotionService) {
+  }
+
+  ngOnInit() {
+    this.promotionservice.getproductPromotion().subscribe(data => this.prd = data);
+  }
+
+}
