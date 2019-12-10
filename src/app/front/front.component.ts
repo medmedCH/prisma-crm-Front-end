5dfbd3d342cca80aba50a07d012097fc588ee463
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-12-10 07:28:00 +0100

    Tawk To , and deconnecte button

--- src/app/front/front.component.ts
+++ src/app/front/front.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {StorageService} from '../services/security/storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-front',
@@ -7,4 +9,11 @@ import {Component} from '@angular/core';
 })
 export class FrontComponent {
   title = 'argon-dashboard-angular';
+
+  constructor(private router: Router) {}
+  userLogged = StorageService.get('currentUser');
+  logout() {
+    this.router.navigateByUrl('/login');
+    StorageService.clear('currentUser');
+  }
 }
